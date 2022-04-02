19f8d849a895272416eb246f4dcd2250353efe41
artinuwq <artin@aroman>
2022-04-02 16:50:07 +0300

    delete dream update overlay XYZ

---
+++ src/main/java/net/mcreator/horrormods/HorrorModsModVariables.java
@@ -0,0 +1,10 @@
+package net.mcreator.horrormods;
+
+public class HorrorModsModVariables {
+	public HorrorModsModVariables(HorrorModsModElements elements) {
+	}
+
+	public static double X = 0;
+	public static double Z = 0;
+	public static double Y = 0;
+}
